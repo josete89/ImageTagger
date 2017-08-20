af6dee0321676fe26056e1d84eb0ad49f3746760
Jose Luis Alcala <joseluis.alcala@adidas-group.com>
2017-08-20 19:12:56 +0200

    Initial Commit

---
+++ ImageTagger/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ImageTagger
+//
+//  Created by Alcala, Jose Luis on 8/20/17.
+//  Copyright © 2017 Alcala, Jose Luis. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
